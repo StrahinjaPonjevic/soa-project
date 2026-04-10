1ab29f56066ee03c451c369d457edd2e8ca3663e
Igor Glisovic <igorognjen6@gmail.com>
2026-04-10 11:45:05 +0200

    register endpoint without jwt

---
+++ Explorer/AuthService/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AuthService.Data;
+using AuthService.DTOs;
+using AuthService.Models;
+
+namespace AuthService.Controllers;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public AuthController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(RegisterDto dto)
+    {
+        // VALIDACIJA ROLE
+        if (dto.Role != "Guide" && dto.Role != "Tourist")
+            return BadRequest("Invalid role");
+
+        // PROVERA DA LI POSTOJI USERNAME
+        if (_context.Users.Any(u => u.Username == dto.Username))
+            return BadRequest("Username already exists");
+
+        var user = new User
+        {
+            Username = dto.Username,
+            Email = dto.Email,
+            Role = dto.Role,
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password)
+        };
+
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        return Ok("User created");
+    }
+}
